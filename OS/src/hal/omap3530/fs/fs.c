7329fe1f48b7cc7097e60c1aa949cac9a5488189
Lukas Ender <hello@lukasender.at>
2013-05-23 14:51:04 +0200

    Again: Introduced fat_io_lib and generic file system structure. Merge fucked up things...

---
+++ OS/src/hal/omap3530/fs/fs.c
@@ -0,0 +1,18 @@
+/*
+ * fs.c
+ *
+ *  Created on: 10.05.2013
+ *      Author: lumannnn
+ */
+
+#include "fs.h"
+#include "../../generic/fs/fs.h"
+
+int fs_init() {
+	fs_detect_mode();
+	// --> select mode.
+}
+
+static int fs_detect_mode() {
+
+}
