cf65738de01720acc9be8f08d31e20fcb2726b1b
Eden33 <eduard.gopp@gmail.com>
2013-06-20 12:30:44 +0200

    DMX driver a fix

--- dmx/dmx.c
+++ dmx/dmx.c
@@ -5,13 +5,33 @@
 #include <os_stdio.h>
 #include <bit.h>
 #include <os_std.h>
+#include <string.h>
 
+
+#define NUMBER_OF_CHANNELS 			256
+uint8_t channels[20];
 driver_msg_t msg;
 handle_t handle;
+
+typedef enum {
+	RED = 2,
+	GREEN = 3,
+	BLUE = 4
+} Color_t;
+
+typedef enum {
+	DMX_COLORSPEC_INTENSIFY = -0x1,
+	DMX_COLORSPEC_SOFTEN = 0x1
+} DMX_Colorspektrum_Flow_t;
+
 
 /*static int dmx_write(void* handle, driver_msg_t* buf, size_t size) {
 	os_write(&handle, &msg, len);
 }*/
+static void dmx_uart_set_send_mode(void);
+static void dmx_uart_set_reset_mode(void);
+static void dmx_send_reset(void);
+static void dmx_send_complete_color_spektrum(int time_in_millis, Color_t color, DMX_Colorspektrum_Flow_t flow);
 
 Driver_t driver = {
 		NULL,
@@ -21,12 +41,12 @@ Driver_t driver = {
 		NULL//dmx_write
 };
 
-#define SCM_CONTROL_PADCONF_MMC2_DAT2 0x48002160
-#define DMX_GPIO5_OE 0x49056034
-#define DMX_GPIO5_DATAOUT 0x4905603C
+#define SCM_CONTROL_PADCONF_MMC2_DAT2 	0x48002160
+#define DMX_GPIO5_OE 					0x49056034
+#define DMX_GPIO5_DATAOUT 				0x4905603C
 
-#define SCM_CONTROL_PADCONF_UART2_TX 0x48002178
-#define SCM_CONTROL_PADCONF_UART2_CTS 0x48002174
+#define SCM_CONTROL_PADCONF_UART2_TX 	0x48002178
+#define SCM_CONTROL_PADCONF_UART2_CTS 	0x48002174
 
 #define SCM_MODE_0					 		0x0
 #define SCM_MODE_1							0x1
@@ -37,7 +57,7 @@ Driver_t driver = {
 #define SCM_MODE_6							0x6
 #define SCM_MODE_7							0x7
 
-void dmx_uart_set_send_mode(void) {
+static void dmx_uart_set_send_mode(void) {
 
 	//Omap3530x.pdf, modes on page 780
 	//Chosen modes: page 96 in BBSRM_latest.pdf
@@ -62,7 +82,7 @@ void dmx_uart_set_send_mode(void) {
  * Set Modes as stated in BBSRM_latest.pdf on page 96 to use
  * Expansion-Board pin 6 with GPIO_146.
  */
-void dmx_uart_set_reset_mode(void) {
+static void dmx_uart_set_reset_mode(void) {
 
 	//Omap3530x.pdf, modes on page 780
 	//Chosen modes: page 96 in BBSRM_latest.pdf
@@ -82,7 +102,7 @@ void dmx_uart_set_reset_mode(void) {
  * Reset: Sending at least 22 bits low (=break) (88 microseconds)
  * then send mark after break (2 bits high) (4 microseconds)
  */
-void dmx_send_reset(void) {
+static void dmx_send_reset(void) {
 	unsigned int addresses[1] = { DMX_GPIO5_DATAOUT };
 	unsigned int values[1];
 
@@ -107,7 +127,30 @@ void dmx_send_reset(void) {
 	for(i = 0; i < a; i++);
 }
 
-uint8_t channels[20];
+static void dmx_send_complete_color_spektrum(int time_in_millis, Color_t color, DMX_Colorspektrum_Flow_t flow) {
+	int sleep_per_color = time_in_millis / 255;
+	if(sleep_per_color < 2) {
+		sleep_per_color = 2;
+	}
+
+	int i;
+	channels[0] = 0x0;	//start byte
+	channels[1] = 0x2; 	//set to RGB
+	channels[2] = 0x0;  //red
+	channels[3] = 0x0;  //green
+	channels[4] = 0x0;  //blue
+	channels[5] = 0x0;	//no function - no speed
+	for(i = 1; i <= 255; i++) {
+
+		sleep(sleep_per_color);
+		dmx_uart_set_reset_mode();
+		dmx_send_reset();
+		dmx_uart_set_send_mode();
+		channels[color] = i;
+		memcpy(msg.buffer, channels, sizeof(channels));
+		os_write(&handle, &msg, sizeof(channels));
+	}
+}
 
 int main(void) {
 	msg.data[0] = 0xC;	//kbaud divider
@@ -123,7 +166,7 @@ int main(void) {
 	
 	unsigned int address[3] = { SCM_CONTROL_PADCONF_MMC2_DAT2, DMX_GPIO5_OE, DMX_GPIO5_DATAOUT };
 	unsigned int values[3];
-	memory_mapped_read(address, 2);
+	memory_mapped_read(address, 3);
 
 	//mode 4 is GPIO
 	values[0] = address[0] & ~0x70000;
@@ -136,22 +179,46 @@ int main(void) {
 	values[2] = address[2] | 0x80; 	//set bit 7 to high, to enable "Driver output" on RS485
 	address[2] = DMX_GPIO5_DATAOUT;
 
-	memory_mapped_write(values, address, 2);
+	memory_mapped_write(values, address, 3);
 
 	channels[0] = 0x0;
 	channels[1] = 0x2;
 	channels[2] = 0x0;
-	channels[3] = 0xB0;
-	channels[4] = 0x0;
+	channels[3] = 0x0;
+	channels[4] = 0xFB;
 	channels[5] = 0x0;
 
 	memcpy(msg.buffer, channels, sizeof(channels));
 
+	int millis = 2000;
+
+//	while (1) {
+//
+//		dmx_send_complete_color_spektrum(millis, BLUE, DMX_COLORSPEC_SOFTEN);
+//		dmx_send_complete_color_spektrum(millis, BLUE, DMX_COLORSPEC_INTENSIFY);
+//
+//		dmx_send_complete_color_spektrum(millis, GREEN, DMX_COLORSPEC_SOFTEN);
+//		dmx_send_complete_color_spektrum(millis, GREEN, DMX_COLORSPEC_INTENSIFY);
+//
+//		dmx_send_complete_color_spektrum(millis, RED, DMX_COLORSPEC_SOFTEN);
+//		dmx_send_complete_color_spektrum(millis, RED, DMX_COLORSPEC_INTENSIFY);
+//
+//	}
+
+	int ctr = 0;
 	while (1) {
+
 		dmx_uart_set_reset_mode();
 		dmx_send_reset();
 		dmx_uart_set_send_mode();
 
+		ctr++;
+		if(ctr > 254)
+			ctr = 0;
+
+//		channels[3] = ctr;
+//		memcpy((void*)msg.buffer[3], (void*) channels[3], 1);
+		msg.buffer[3] = ctr;
 		os_write(&handle, &msg, sizeof(channels));
 
 		sleep(2);
